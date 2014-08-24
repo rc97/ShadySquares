5887205c181c29d1b6dd644485a7aa531becd12f
Rupesh Chinta <rupesh.ch97@gmail.com>
2014-08-24 19:55:09 -0400

    Added Icons

--- gen/com/example/shadysquares/R.java
+++ gen/com/example/shadysquares/R.java
@@ -1080,6 +1080,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
         public static final int im2=0x7f020058;
         public static final int search_icon=0x7f020059;
+        public static final int shady_squares=0x7f02005a;
     }
     public static final class id {
         public static final int L1=0x7f05003d;
